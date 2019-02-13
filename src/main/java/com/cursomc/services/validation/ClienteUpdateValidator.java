284c466df5c6c0fcf00d010ddb43e9a89ef6a497
Ismael MS <ims.86@hotmail.com>
2019-02-13 18:27:54 -0200

    Validacao email no PUT

---
+++ src/main/java/com/cursomc/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,50 @@
+package com.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.cursomc.domain.Cliente;
+import com.cursomc.dto.ClienteDTO;
+import com.cursomc.repositories.ClienteRepository;
+import com.cursomc.resources.exceptions.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		
+		List<FieldMessage> list = new ArrayList<>();
+				
+		Cliente aux =  repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já cadastrado"));
+		}
+		
+		// Percorre a lista personalizada para associala na lista de erros do frame	
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
