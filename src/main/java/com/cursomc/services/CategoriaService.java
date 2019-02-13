e02e1580defce83941eb2c98fda317ce1b718c6b
Ismael MS <ims.86@hotmail.com>
2019-02-13 11:37:24 -0200

    Validacao e personalizacao do retorno

--- src/main/java/com/cursomc/services/CategoriaService.java
+++ src/main/java/com/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.cursomc.domain.Categoria;
+import com.cursomc.dto.CategoriaDTO;
 import com.cursomc.repositories.CategoriaRepository;
 import com.cursomc.services.exception.DataIntegrityExceptions;
 import com.cursomc.services.exception.ObjecNotFoundExceptions;
@@ -55,6 +56,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linsPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linsPerPage,Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
